fb5bd332d2651878c057ae4d7b4a2895cef0513d
NithishK2002 <93331531+NithishK2002@users.noreply.github.com>
2023-07-04 14:44:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask , render_template
-
+from flask import Flask
+  
 app = Flask(__name__)
-
-
-@app.route('/')
+  
+# Pass the required route to the decorator.
+@app.route("/hello")
+def hello():
+    return "Hello, Welcome to GeeksForGeeks"
+    
+@app.route("/")
 def index():
-    return render_template("index.html")
-
-
-app.run(host='0.0.0.0', port=81)
+    return "Homepage of GeeksForGeeks"
+  
+if __name__ == "__main__":
+    app.run(debug=True)
